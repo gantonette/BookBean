f63b63c4945635d2dc423aba8cb84b0ce9674e9f
gantonette <62356789+gantonette@users.noreply.github.com>
2023-07-16 16:52:12 +1200

    even EVEN more perfect- s3 image CRUD added for bookEntryImage

--- src/main/java/com/github/gantonette/bookbean/controller/BookEntryController.java
+++ src/main/java/com/github/gantonette/bookbean/controller/BookEntryController.java
@@ -3,7 +3,10 @@ package com.github.gantonette.bookbean.controller;
 import com.github.gantonette.bookbean.model.BookEntry;
 import com.github.gantonette.bookbean.service.BookEntryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -38,5 +41,19 @@ public class BookEntryController {
         bookEntryService.deleteBookEntry(entryId);
     }
 
+    @GetMapping("/books/{bookId}/entries/{entryId}/image")
+    @ResponseBody
+    ResponseEntity<InputStreamResource> getBookEntryImage(@PathVariable String bookId, @PathVariable String entryId) {
+        return bookEntryService.getBookEntryImage(entryId);
+    }
 
+    @PostMapping("/books/{bookId}/entries/{entryId}/image")
+    void addBookEntryImage(@PathVariable String entryId, @PathVariable String bookId, @RequestPart MultipartFile image) {
+        bookEntryService.addBookEntryImage(entryId, image);
+    }
+
+    @DeleteMapping("/books/{bookId}/entries/{entryId}/image/")
+    void deleteBookEntryImage(@PathVariable String entryId, @PathVariable String bookId) {
+        bookEntryService.deleteBookEntryImage(entryId);
+    }
 }
--- src/main/java/com/github/gantonette/bookbean/service/BookEntryService.java
+++ src/main/java/com/github/gantonette/bookbean/service/BookEntryService.java
@@ -3,9 +3,16 @@ package com.github.gantonette.bookbean.service;
 import com.github.gantonette.bookbean.model.Book;
 import com.github.gantonette.bookbean.model.BookEntry;
 import io.awspring.cloud.dynamodb.DynamoDbTemplate;
+import io.awspring.cloud.s3.S3Resource;
+import io.awspring.cloud.s3.S3Template;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 import software.amazon.awssdk.enhanced.dynamodb.Expression;
 import software.amazon.awssdk.enhanced.dynamodb.model.PageIterable;
@@ -14,6 +21,7 @@ import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
 import software.amazon.awssdk.enhanced.dynamodb.model.ScanEnhancedRequest;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -22,7 +30,9 @@ import java.util.UUID;
 @Service
 public class BookEntryService {
 
+    private final String BUCKET_NAME = "book-entry-files";
     @Autowired DynamoDbTemplate dynamoDbTemplate;
+    @Autowired S3Template s3Template;
 
     public List<BookEntry> getBookEntries(String bookId) {
         ScanEnhancedRequest request;
@@ -66,6 +76,7 @@ public class BookEntryService {
     public BookEntry postBookEntry(String bookId, BookEntry bookEntry) {
         bookEntry.setBookId(bookId);
         String entryId = UUID.randomUUID().toString();
+        bookEntry.setImageRef(null);
         bookEntry.setBookEntryId(entryId);
         dynamoDbTemplate.save(bookEntry);
         return bookEntry;
@@ -84,5 +95,49 @@ public class BookEntryService {
     public void deleteBookEntry(String entryId) {
         BookEntry existingBookEntry = getBookEntry(entryId);
         dynamoDbTemplate.delete(existingBookEntry);
+    }
+
+    public ResponseEntity<InputStreamResource> getBookEntryImage(String entryId) {
+        try {
+            BookEntry existingBookEntry = getBookEntry(entryId);
+            if (existingBookEntry.getImageRef() == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "image does not exist");
+            }
+            S3Resource image = s3Template.download(BUCKET_NAME, existingBookEntry.getImageRef());
+
+            HttpHeaders headers = new HttpHeaders();
+            InputStreamResource streamSource = new InputStreamResource(image.getInputStream());
+            headers.setContentLength(image.contentLength());
+            headers.setContentType(MediaType.valueOf(image.contentType()));
+            return new ResponseEntity<>(streamSource, headers, HttpStatus.OK);
+        } catch (ResponseStatusException | IOException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Book entry not found");
+        }
+    }
+
+    public void addBookEntryImage(String entryId, MultipartFile image) {
+        try {
+            BookEntry existingBookEntry = getBookEntry(entryId);
+            String imageRef = existingBookEntry.getBookEntryId() + "-" + image.getOriginalFilename();
+            s3Template.upload(BUCKET_NAME, imageRef, image.getInputStream());
+            existingBookEntry.setImageRef(imageRef);
+            dynamoDbTemplate.update(existingBookEntry);
+        } catch (ResponseStatusException | IOException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Book not found");
+        }
+
+    }
+    public void deleteBookEntryImage(String entryId) {
+        try {
+            BookEntry existingBookEntry = getBookEntry(entryId);
+            if (existingBookEntry.getImageRef() == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Image not found");
+            }
+            s3Template.deleteObject(BUCKET_NAME, existingBookEntry.getImageRef());
+            existingBookEntry.setImageRef(null);
+            dynamoDbTemplate.update(existingBookEntry);
+        } catch (ResponseStatusException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Book entry not found");
+        }
     }
 }
